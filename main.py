d83984f159ff6086e372d955bdea2634f8299d12
Josiah Henson <pbinspanish@outlook.com>
2023-03-27 10:37:58 -0400

    add scan options

--- main.py
+++ main.py
@@ -10,19 +10,30 @@ def main():
   matching_tags = config.get("matching_tags")
   modification_tags = config.get("modification_tags")
   
-  # Generate YAML
-  modules.generator.generate_yaml(settings.get("root_directory"), settings.get("save_directory"), settings.get("track_identifier"), matching_tags, modification_tags)
+  # Generate list of files to edit
+  music_files = []
+  file_types = matching_tags.keys()
+  if settings.get("scan_mode") == "all":
+    for root, dirs, files in os.walk(settings.get("root_directory")):
+      for file in files:
+        for file_type in file_types:
+          if file.endswith(file_type):
+            music_files.append(os.path.join(root, file))
   
-  # Test
-  # gets only files edited in the last day
-  for root, dirs, files in os.walk(settings.get("root_directory")):
-    for file in files:
-        full_path = os.path.join(root, file)
-        modified_time = os.path.getmtime(full_path)
-        if modified_time:
-            modified_time = datetime.datetime.fromtimestamp(modified_time)
-            if (datetime.datetime.now() - modified_time).days < 1:
-                print(full_path)
+  elif settings.get("scan_mode") == "modified":
+    for root, dirs, files in os.walk(settings.get("root_directory")):
+      for file in files:
+        for file_type in file_types:
+          if file.endswith(file_type):
+            full_path = os.path.join(root, file)
+            modified_time = os.path.getmtime(full_path)
+            if modified_time:
+                modified_time = datetime.datetime.fromtimestamp(modified_time)
+                if (datetime.datetime.now() - modified_time).days < settings.get("modified_time_days"):
+                    music_files.append(full_path)
+  
+  # Generate YAML
+  modules.generator.generate_yaml(music_files, settings.get("root_directory"), settings.get("save_directory"), settings.get("track_identifier"), matching_tags, modification_tags)
 
 if __name__ == "__main__":
   main()
--- modules/generator.py
+++ modules/generator.py
@@ -9,7 +9,7 @@ def read_tag(file_path, tag):
     # Return the tag if it exists, otherwise return an empty string
     return " ".join(audio.tags[tag]) if tag in audio.tags else ""
 
-def generate_yaml(root_dir, save_dir, track_identifier, matching_tags, modification_tags):
+def generate_yaml(files, root_dir, save_dir, track_identifier, matching_tags, modification_tags):
     # Config
     file_types = matching_tags.keys() # assuming all file types show up in both tag configs.
     
@@ -17,55 +17,54 @@ def generate_yaml(root_dir, save_dir, track_identifier, matching_tags, modificat
     metadata = {}
 
     # Loop through every music file in the root directory and its subdirectories
-    for root, dirs, files in os.walk(root_dir):
-        for file in files:
-            # Check if the file is a music file
-            if file.endswith(".mp3") or file.endswith(".flac") or file.endswith(".ogg"):
-                # Read Vorbis Comments
-                if file.endswith(".flac") or file.endswith(".ogg"):
-                    display_artist = read_tag(os.path.join(root, file), "DISPLAY ARTIST")
-                    album_artist = read_tag(os.path.join(root, file), "ALBUMARTIST")
-                    album_title = read_tag(os.path.join(root, file), "ALBUM")
-                    track_title = read_tag(os.path.join(root, file), "TITLE")
-                    itunes_advisory = read_tag(os.path.join(root, file), "ITUNESADVISORY")
-                    track_number = read_tag(os.path.join(root, file), "TRACKNUMBER")
-                # ID3v2 tags
-                if file.endswith(".mp3"):
-                    display_artist = read_tag(os.path.join(root, file), "TXXX:DISPLAY ARTIST")
-                    album_artist = read_tag(os.path.join(root, file), "TPE2")
-                    album_title = read_tag(os.path.join(root, file), "TALB")
-                    track_title = read_tag(os.path.join(root, file), "TIT2")
-                    itunes_advisory = read_tag(os.path.join(root, file), "TXXX:ITUNESADVISORY")
-                    track_number = read_tag(os.path.join(root, file), "TRCK")
+    for file in files:
+        # Check if the file is a music file
+        if file.endswith(".mp3") or file.endswith(".flac") or file.endswith(".ogg"):
+            # Read Vorbis Comments
+            if file.endswith(".flac") or file.endswith(".ogg"):
+                display_artist = read_tag(file, "DISPLAY ARTIST")
+                album_artist = read_tag(file, "ALBUMARTIST")
+                album_title = read_tag(file, "ALBUM")
+                track_title = read_tag(file, "TITLE")
+                itunes_advisory = read_tag(file, "ITUNESADVISORY")
+                track_number = read_tag(file, "TRACKNUMBER")
+            # ID3v2 tags
+            if file.endswith(".mp3"):
+                display_artist = read_tag(file, "TXXX:DISPLAY ARTIST")
+                album_artist = read_tag(file, "TPE2")
+                album_title = read_tag(file, "TALB")
+                track_title = read_tag(file, "TIT2")
+                itunes_advisory = read_tag(file, "TXXX:ITUNESADVISORY")
+                track_number = read_tag(file, "TRCK")
 
-                # Add explicit indicator to track
-                mod_track_title = ""
-                if itunes_advisory == "1":
-                    mod_track_title = track_title + " " + u"\U0001F174"
+            # Add explicit indicator to track
+            mod_track_title = ""
+            if itunes_advisory == "1":
+                mod_track_title = track_title + " " + u"\U0001F174"
 
-                # Add the display artist to the metadata dictionary
-                if album_artist not in metadata:
-                    metadata[album_artist] = {}                         # adds an empty dictionary at the album artist
-                    metadata[album_artist]["albums"] = {}               # adds the albums key and creates an empty dictionary there
-                    
-                if album_title not in metadata[album_artist]["albums"]:
-                    metadata[album_artist]["albums"][album_title] = {}
-                    metadata[album_artist]["albums"][album_title]["tracks"] = {}
+            # Add the display artist to the metadata dictionary
+            if album_artist not in metadata:
+                metadata[album_artist] = {}                         # adds an empty dictionary at the album artist
+                metadata[album_artist]["albums"] = {}               # adds the albums key and creates an empty dictionary there
                 
-                if track_identifier == "track_number":
-                    if track_number not in metadata[album_artist]["albums"][album_title]["tracks"]:
-                        metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])] = {}
-                    metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])]["original_artist"] = display_artist
-                    metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])]["title"] = mod_track_title
-                
-                elif track_identifier == "track_title":
-                    if track_number not in metadata[album_artist]["albums"][album_title]["tracks"]:
-                        metadata[album_artist]["albums"][album_title]["tracks"][track_title] = {}
-                    metadata[album_artist]["albums"][album_title]["tracks"][track_title]["original_artist"] = display_artist
-                    metadata[album_artist]["albums"][album_title]["tracks"][track_title]["title"] = mod_track_title
+            if album_title not in metadata[album_artist]["albums"]:
+                metadata[album_artist]["albums"][album_title] = {}
+                metadata[album_artist]["albums"][album_title]["tracks"] = {}
+            
+            if track_identifier == "track_number":
+                if track_number not in metadata[album_artist]["albums"][album_title]["tracks"]:
+                    metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])] = {}
+                metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])]["original_artist"] = display_artist
+                metadata[album_artist]["albums"][album_title]["tracks"][int(track_number.split("/")[0])]["title"] = mod_track_title
+            
+            elif track_identifier == "track_title":
+                if track_number not in metadata[album_artist]["albums"][album_title]["tracks"]:
+                    metadata[album_artist]["albums"][album_title]["tracks"][track_title] = {}
+                metadata[album_artist]["albums"][album_title]["tracks"][track_title]["original_artist"] = display_artist
+                metadata[album_artist]["albums"][album_title]["tracks"][track_title]["title"] = mod_track_title
 
-                
-                print(track_number + " " + track_title + " - " + display_artist)
+            
+            print(track_number + " " + track_title + " - " + display_artist)
 
 
     # Wrap the metadata dictionary in another dictionary
