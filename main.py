9aba37fdc1ad7339011568656a68219b19a8425f
Josiah Henson <pbinspanish@outlook.com>
2023-03-30 10:00:50 -0400

    split file scanner from main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
+import modules.generator as generator
+import modules.scanner as scanner
 import yaml
-import modules.generator
-import datetime
 
 def main():
   # Load Config
@@ -12,29 +11,10 @@ def main():
   read_tags = config.get("read_tags")
   
   # Generate list of files to edit
-  music_files = []
-  file_types = matching_tags.keys()
-  if settings.get("scan_mode") == "all":
-    for root, dirs, files in os.walk(settings.get("root_directory")):
-      for file in files:
-        for file_type in file_types:
-          if file.endswith(file_type):
-            music_files.append(os.path.join(root, file))
-  
-  elif settings.get("scan_mode") == "modified":
-    for root, dirs, files in os.walk(settings.get("root_directory")):
-      for file in files:
-        for file_type in file_types:
-          if file.endswith(file_type):
-            full_path = os.path.join(root, file)
-            modified_time = os.path.getmtime(full_path)
-            if modified_time:
-                modified_time = datetime.datetime.fromtimestamp(modified_time)
-                if (datetime.datetime.now() - modified_time).days < settings.get("modified_time_days"):
-                    music_files.append(full_path)
+  music_files = scanner.scan_files(settings.get("root_directory"), settings.get("scan_mode"), settings.get("modified_time_days"), matching_tags)
   
   # Generate YAML
-  modules.generator.generate_yaml(music_files, settings.get("save_directory"), settings.get("track_identifier"), settings.get("explicit_character"), read_tags, matching_tags, modification_tags)
+  generator.generate_yaml(music_files, settings.get("save_directory"), settings.get("track_identifier"), settings.get("explicit_character"), read_tags, matching_tags, modification_tags)
 
 if __name__ == "__main__":
   main()
